c53683e885e8dfb6d3db4d495303f756c216d4ad
QQais <Qais.Qaisi@revature.net>
2021-07-27 16:05:57 -0500

    Statistics TopEarned coins , Top Spent Coins , Top Balance

--- Statistics-and-Leaderboard/P3_Statistics_API/P3_Statisitcs_Testing/DemoTest.cs
+++ Statistics-and-Leaderboard/P3_Statistics_API/P3_Statisitcs_Testing/DemoTest.cs
@@ -4,39 +4,200 @@ using System;
 using Xunit;
 using System.Linq;
 using System.Threading.Tasks;
+using BuisinessLayerMethods;
+using System.Collections.Generic;
 
 namespace P3_Statisitcs_Testing
 {
     public class DemoTest
     {
         DbContextOptions<P3Context> options = new DbContextOptionsBuilder<P3Context>().UseInMemoryDatabase(databaseName: "testingDb").Options;
+      
+
+        //================================List Top Coins Balance============================
         [Fact]
-        public async Task SomeTestMethodAsync()
+        public void TopCurrentBallanceTest()
         {
             //Arrange
-            User user = new()
+
+            string error;
+            
+            // Create a dummy user 
+            User user1 = new User()
             {
-                UserId = 1,
+                UserId =1,
+                CoinBalance=5,
+                TotalCoinsEarned=10
             };
 
+            // Create a dummy user 
+            User user2 = new User()
+            {
+                UserId = 2,
+                CoinBalance = 2,
+                TotalCoinsEarned = 100
+            };
+
+            // Create a dummy user 
+            User user3 = new User()
+            {
+                UserId = 3,
+                CoinBalance = 7,
+                TotalCoinsEarned = 200
+            };
+
+
+
             //Act
-            //bool result = false;
             using (var context = new P3Context(options))
             {
-                context.Database.EnsureDeleted();
-                context.Database.EnsureCreated();
+                //verify that the db was deleted and created anew
+                context.Database.EnsureDeleted();//delete any Db from a previous test
 
-                //some method perhaps
+                context.Database.EnsureCreated();// create a new Db.. you will need to seed it again.
+
+                LeaderboardModel usertest = new LeaderboardModel(context);
+
+                context.Users.Add(user1);
+                context.Users.Add(user2);
+                context.Users.Add(user3);
+
 
                 context.SaveChanges();
-            }
+                //Assert
 
-            //Assert
-            using (var context = new P3Context(options))
-            {
-                //assert things here
-                //Assert.True(result);
+                List<User> list1 = usertest.TopCurrentBallance(2);
+
+                Assert.NotNull(list1);
+                Assert.Equal(2, list1.Count());
             }
         }
+
+
+        //================================List Top Earned Coins ============================
+        [Fact]
+        public void TopEarnedCoinsTest()
+        {
+            //Arrange
+
+            string error;
+
+            // Create a dummy user 
+            User user1 = new User()
+            {
+                UserId = 1,
+                CoinBalance = 5,
+                TotalCoinsEarned = 10
+            };
+
+            // Create a dummy user 
+            User user2 = new User()
+            {
+                UserId = 2,
+                CoinBalance = 2,
+                TotalCoinsEarned = 100
+            };
+
+            // Create a dummy user 
+            User user3 = new User()
+            {
+                UserId = 3,
+                CoinBalance = 7,
+                TotalCoinsEarned = 200
+            };
+
+
+
+            //Act
+            using (var context = new P3Context(options))
+            {
+                //verify that the db was deleted and created anew
+                context.Database.EnsureDeleted();//delete any Db from a previous test
+
+                context.Database.EnsureCreated();// create a new Db.. you will need to seed it again.
+
+                LeaderboardModel usertest = new LeaderboardModel(context);
+
+                context.Users.Add(user1);
+                context.Users.Add(user2);
+                context.Users.Add(user3);
+
+
+                context.SaveChanges();
+                //Assert
+
+                List<User> list1 = usertest.TopEarnedCoins(2);
+
+                Assert.NotNull(list1);
+                Assert.Equal(2, list1.Count());
+            }
+        }
+
+
+
+        //================================List Top Spent Coins ============================
+        [Fact]
+        public void TopSpentCoinsTest()
+        {
+            //Arrange
+
+            string error;
+
+            // Create a dummy user 
+            User user1 = new User()
+            {
+                UserId = 1,
+                CoinBalance = 5,
+                TotalCoinsEarned = 10
+            };
+
+            // Create a dummy user 
+            User user2 = new User()
+            {
+                UserId = 2,
+                CoinBalance = 2,
+                TotalCoinsEarned = 100
+            };
+
+            // Create a dummy user 
+            User user3 = new User()
+            {
+                UserId = 3,
+                CoinBalance = 7,
+                TotalCoinsEarned = 200
+            };
+
+
+
+            //Act
+            using (var context = new P3Context(options))
+            {
+                //verify that the db was deleted and created anew
+                context.Database.EnsureDeleted();//delete any Db from a previous test
+
+                context.Database.EnsureCreated();// create a new Db.. you will need to seed it again.
+
+                LeaderboardModel usertest = new LeaderboardModel(context);
+
+                context.Users.Add(user1);
+                context.Users.Add(user2);
+                context.Users.Add(user3);
+
+
+                context.SaveChanges();
+                //Assert
+
+                List<User> list1 = usertest.TopSpentCoins(2);
+
+                Assert.NotNull(list1);
+                Assert.Equal(2, list1.Count());
+            }
+        }
+
+
+
+
+
+
     }
 }
