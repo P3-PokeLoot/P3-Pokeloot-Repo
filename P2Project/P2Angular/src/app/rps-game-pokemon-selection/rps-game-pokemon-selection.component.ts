d4ad95c01a339eaa5fc98633999f247910c75945
Malia-Labor <malia.labor@revature.net>
2021-07-28 07:49:09 -0700

    adds pokemon RPS selection component

---
+++ P2Project/P2Angular/src/app/rps-game-pokemon-selection/rps-game-pokemon-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rps-game-pokemon-selection',
+  templateUrl: './rps-game-pokemon-selection.component.html',
+  styleUrls: ['./rps-game-pokemon-selection.component.css']
+})
+export class RpsGamePokemonSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
