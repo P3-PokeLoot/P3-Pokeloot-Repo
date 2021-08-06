bdbe38e0211c442b85f66a1e28671a0087bcc49b
GregoryAgnew <gregory.agnew@revature.net>
2021-08-06 16:39:19 -0500

    change docker stuff

--- P2Project/P2Angular/src/environments/environment.prod.ts
+++ P2Project/P2Angular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  urlstat: "http://20.106.64.124/statistic/api",
-  urlmain: "http://20.106.64.124/main/api",
-  urlmainlocalonly: "http://20.106.64.124/main/api",
-  urlgame: "http://20.106.64.124/game/api"
+  urlstat: "http://statistic/api/",
+  urlmain: "http://main/api/",
+  urlmainlocalonly: "http://main/api/",
+  urlgame: "http://game/api/"
 };                                                                                                    
