065f1b2cda7a57c6508d8228052416216552c882
ChristianRevature <christian.romero@revature.net>
2021-08-09 19:41:25 -0400

    adds models for LB service

---
+++ P2Project/P2Angular/src/app/Models/IUser_RarityLB.ts
@@ -0,0 +1,9 @@
+export interface UserModel_Rarity{
+    UserId:number,
+    UserName:string,
+    TotalCommon:number,
+    TotalUncommon:number,
+    TotalRare:number,
+    TotalMythic:number,
+    TotalLegendary:number
+}
