301e09731ecf004d1e33fd5a97615c92877a3ccd
BrianCorbett <brian.corbett@revature.net>
2021-08-10 17:28:08 -0700

    restyling of home posts page

--- P2Project/P2Angular/src/app/posts/posts.component.ts
+++ P2Project/P2Angular/src/app/posts/posts.component.ts
@@ -239,9 +239,11 @@ export class PostsComponent implements OnInit {
         console.log(result);
         },
       error => {
+        console.log(error.error);
         //From Alain: for some reason the call is returning an error due to parsing issues, the status code is 200 and the database is succefully updated on each call.
         //Since the issue is unresolved, i instead grabbed the error text which actually holds the output intended from the api
         this.friendAction = error.error.text;
+        console.log(this.friendAction);
       }
     );
     }
